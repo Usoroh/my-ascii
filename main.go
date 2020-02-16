52d36f7b368a7d4eab5ddde1c82a329d59d05d57
Usoroh <paius.usoroh@gmail.com>
2020-02-16 23:16:19 +0600

    change newline handling

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"strings"
+	// "strings"
 	"fmt"
 
 	ascii "./ascii"
@@ -15,16 +15,22 @@ type fillerText struct {
 	Text string
 }
 
+var wAdress *http.ResponseWriter
+
+
 func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if r.Method == "GET" {
+		wAdress = &w
 		if r.URL.Path != "/" {
 			fmt.Println(404)
 			t, _ := template.ParseFiles("404.html")
 			t.Execute(w, nil)
 			return
 		}
+		// fmt.Println(w)
 		t, _ := template.ParseFiles("index.html")
+		fmt.Println(t)
 		t.Execute(w, nil)
 	}
 }
@@ -32,34 +38,40 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 func chooseFont(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
-
-
 	if r.Method == "GET" {
 
 		str := r.Form["string"][0]
 		f := r.Form["font"][0]
-
-		newInput := strings.Replace(str, "\r\n", "\\n", -1)
-		// args := []string{str, f}
-		// txt := fillerText{Text: Font(args)}
-		rStr, status := ascii.FontAscii(newInput, f)
+		// newInput := strings.Replace(str, "\n", "\n", -1)
+		rStr, status := ascii.FontAscii(str, f)
 		if status == 500 {
-			t, err := template.ParseFiles("500.html")
-			if err != nil {
-				fmt.Println(err)
-			}
-			t.Execute(w, nil)
+			e := fillerText{Text: "500"}
+			js, _ := json.Marshal(e)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
 			fmt.Println("Internal server error")
-			return
 		} else {
 			txt := fillerText{Text: rStr}
 			js, _ := json.Marshal(txt)
+			fmt.Println(string(js))
 			w.Header().Set("Content-Type", "application/json")
 			w.Write(js)
 		}
 	}
 }
 
+// func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+// 	w.WriteHeader(status)
+// 	if status == 500 {
+// 		fmt.Fprint(w, "Custom 500")
+// 		t, _ := template.ParseFiles("500.html")
+// 		t.Execute(w, nil)
+// 	}
+
+// 	// fmt.Println(name)
+// 	// t, _ := template.ParseFiles(name+".html")
+// 	// t.Execute(w, nil)
+// }
 
 func main() {
 	http.HandleFunc("/", serveTemplate)
